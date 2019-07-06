06585b63d2b3dc9d1d7fb7aed72ee1e7c8ebc408
smk87 <30999133+smk87@users.noreply.github.com>
2019-07-06 13:49:32 +0600

    conditionals,loops done.

--- conditionals.py
+++ conditionals.py
@@ -12,8 +12,25 @@
 
 
 # Membership Operators (not, not in) - Membership operators are used to test if a sequence is presented in an object
+numbers=[1,2,3,4]
+x=3
+y=10
 
+# In
+if(x in numbers):
+    print(x in numbers)
 
+#Not In
+if(y not in numbers):
+    print(y in numbers)
 
 
 # Identity Operators (is, is not) - Compare the objects, not if they are equal, but if they are actually the same object, with the same memory location:
+
+#Is
+if x is x:
+    print(x is y)
+
+#Is not
+if x is not y:
+    print(x is not y)    
--- loops.py
+++ loops.py
@@ -1,5 +1,26 @@
 # A for loop is used for iterating over a sequence (that is either a list, a tuple, a dictionary, a set, or a string).
+people=['John','Saqlain']
 
+for person in people:
+    print('Curretn persosn:',person)
+
+#Break
+for person in people:
+    print('Curretn persosn:',person)
+    break
+
+#Continue
+for person in people:
+    print('Curretn persosn:', person)
+    continue
+
+#Range
+for i in range(len(people)):
+    print('Current person:',people[i])
 
 
 # While loops execute a set of statements as long as a condition is true.
+count=0
+while count<=10:
+    print('Count:',count)
+    count +=1
